3267b014f5a8cdc01f64b979a62b97f2e29db62d
vferniker13 <artemo2311@gmail.com>
2025-09-10 19:40:16 +0300

    добавил изменения

--- main.py
+++ main.py
@@ -18,6 +18,17 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN and not drawing:
             start_position = event.pos
             drawing = True
+        if event.type == pygame.MOUSEBUTTONDOWN and drawing:
+            end_position = event.pos
+            left = min(start_position[0], end_position[0])
+            top = min(start_position[1], end_position[1])
+            width = abs(end_position[0] - start_position[0])
+            height = abs(end_position[1] - start_position[1])
+
+            rectangle = pygame.Rect(left, top, width, height)
+            pygame.draw.rect(screen, (107, 142, 35), rectangle)
+            pygame.display.update()
+            
         if event.type == pygame.MOUSEBUTTONUP and drawing:
             end_position = event.pos
             drawing = False
