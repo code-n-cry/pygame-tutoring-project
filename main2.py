63742be3af5750ae9cca595b13ac90c0ff0ee229
code-n-cry <egor.schukiv@gmail.com>
2025-09-12 18:30:54 +0300

    движение

--- main2.py
+++ main2.py
@@ -12,18 +12,22 @@ start_position = None
 end_position = None
 drawing = False
 running = True
-speed_x = 5
+speed_x = 10
 rect = pygame.Rect(10, 500, 100, 100)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_a and rect.x > 0:
+                rect.x -= speed_x
+            if event.key == pygame.K_d and rect.x + 100 < WIDTH:
                 rect.x += speed_x
         pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[pygame.K_d]:
+        if pressed_keys[pygame.K_d] and rect.x + 100 < WIDTH:
             rect.x += speed_x
+        if pressed_keys[pygame.K_a] and rect.x > 0:
+            rect.x -= speed_x
         screen.fill((0, 0, 0))
         pygame.draw.rect(screen, (107, 142, 35), rect)
         clock.tick(60)
