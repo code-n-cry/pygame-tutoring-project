d3ed51e3996664f1f0a0feb57d86198c4eff7886
vferniker13 <artemo2311@gmail.com>
2025-09-12 19:29:31 +0300

    добал автоматическое передвежение

--- main2.py
+++ main2.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -12,11 +13,17 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.rect = pygame.Rect(10, 500, 100, 100)
-        self.speed_x = 10
-        self.speed_y = 10
+        self.speed_x = random.randint(5,15)
+        self.speed_y = random.randint(5,15)
 
     def update(self):
-        pressed_keys = pygame.key.get_pressed()
+        self.rect.x += self.speed_x
+        self.rect.y -= self.speed_y
+        if self.rect.x + 100 > WIDTH or self.rect.x < 0:
+            self.speed_x = -self.speed_x
+        if  self.rect.y < 0 or self.rect.y + 100 > HEIGHT:
+            self.speed_y = -self.speed_y
+        '''pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_d] and self.rect.x + 100 < WIDTH:
             self.rect.x += self.speed_x
         if pressed_keys[pygame.K_a] and self.rect.x > 0:
@@ -24,7 +31,8 @@ class Player(pygame.sprite.Sprite):
         if pressed_keys[pygame.K_w] and self.rect.y > 0:
             self.rect.y -= self.speed_y
         if pressed_keys[pygame.K_s] and self.rect.y + 100 < HEIGHT:
-            self.rect.y += self.speed_y
+            self.rect.y += self.speed_y'''
+
 
 
 player = Player()
