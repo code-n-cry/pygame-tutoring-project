2042a1141441b56f8bac6cebdbee90c1e8d59fa5
code-n-cry <egor.schukiv@gmail.com>
2025-09-12 19:04:21 +0300

    Добавили спрайты

--- main2.py
+++ main2.py
@@ -8,33 +8,34 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Наша первая игра')
 
-start_position = None
-end_position = None
-drawing = False
-running = True
-speed_x = 10
-speed_y = 10
-rect = pygame.Rect(10, 500, 100, 100)
-while running:
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.rect = pygame.Rect(10, 500, 100, 100)
+        self.speed_x = 10
+        self.speed_y = 10
+
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[pygame.K_d] and self.rect.x + 100 < WIDTH:
+            self.rect.x += self.speed_x
+        if pressed_keys[pygame.K_a] and self.rect.x > 0:
+            self.rect.x -= self.speed_x
+        if pressed_keys[pygame.K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed_y
+        if pressed_keys[pygame.K_s] and self.rect.y + 100 < HEIGHT:
+            self.rect.y += self.speed_y
+
+
+player = Player()
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a and rect.x > 0:
-                rect.x -= speed_x
-            if event.key == pygame.K_d and rect.x + 100 < WIDTH:
-                rect.x += speed_x
-        pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[pygame.K_d] and rect.x + 100 < WIDTH:
-            rect.x += speed_x
-        if pressed_keys[pygame.K_a] and rect.x > 0:
-            rect.x -= speed_x
-        if pressed_keys[pygame.K_w] and rect.y > 0:
-            rect.y -= speed_y
-        if pressed_keys[pygame.K_s] and rect.y + 100 < HEIGHT:
-            rect.y += speed_y
-        screen.fill((0, 0, 0))
-        pygame.draw.rect(screen, (107, 142, 35), rect)
-        clock.tick(60)
-        pygame.display.flip()
-pygame.quit()
+            pygame.quit()
+    player.update()
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (255, 255, 255), player.rect)
+    clock.tick(60)
+    pygame.display.flip()
+
