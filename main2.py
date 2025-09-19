1b3ac2a668c2ef8b8bb54a19e4b30cb39cd9c8c7
code-n-cry <egor.schukiv@gmail.com>
2025-09-19 19:24:05 +0300

    Добавили класс игры

--- main2.py
+++ main2.py
@@ -9,16 +9,41 @@ clock = pygame.time.Clock()
 font = pygame.font.SysFont('Arial', 50)
 
 SPAWN_EVENT = pygame.USEREVENT + 1
-kill_count = 0
 pygame.time.set_timer(SPAWN_EVENT, 3000)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Наша первая игра')
 bg_image = pygame.image.load('background.png')
 bg_image = pygame.transform.scale(bg_image, (WIDTH, HEIGHT))
-in_menu = True
-pressed = False
 
-start_btn = pygame.rect.Rect(300, 270, 230, 50)
+
+class Menu:
+    def __init__(self):
+        self.start_btn = pygame.rect.Rect(300, 270, 230, 50)
+        self.btn_text = font.render('Начать игру', True, (255, 255, 255))
+        self.menu_text = font.render('Меню', True, (255, 255, 255))
+        self.pressed = False
+        self.in_menu = True
+
+    def update(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = event.pos
+            if 300 <= pos[0] <= 530 and 270 <= pos[1] <= 320:
+                self.in_menu = False
+        if event.type == pygame.MOUSEMOTION:
+            pos = pygame.mouse.get_pos()
+            if 300 <= pos[0] <= 530 and 270 <= pos[1] <= 320:
+                self.pressed = True
+            else:
+                self.pressed = False
+
+    def draw(self, screen):
+        screen.fill((0, 0, 0))
+        screen.blit(self.menu_text, (330, 150))
+        if self.pressed is False:
+            pygame.draw.rect(screen, (255, 0, 0), self.start_btn)
+        else:
+            pygame.draw.rect(screen, (0, 0, 255), self.start_btn)
+        screen.blit(self.btn_text, (300, 270))
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -81,75 +106,72 @@ class Player(pygame.sprite.Sprite):
             pygame.draw.rect(screen, (90, 168, 25), self.rect)
 
 
-player = Player(50, 448)
-enemy_group = pygame.sprite.Group()
-bullet_group = pygame.sprite.Group()
-enemy_count = 5
-btn_text = font.render('Начать игру', True, (255, 255, 255))
-btn_menu = font.render('меню', True, (255, 255, 255))
+class Game:
+    def __init__(self):
+        self.player = Player(50, 448)
+        self.menu = Menu()
+        self.enemy_group = pygame.sprite.Group()
+        self.bullet_group = pygame.sprite.Group()
+        self.current_enemies = 3
+        self.kill_count = 0
+
+    def update(self, event):
+        if self.menu.in_menu is True:
+            self.menu.update(event)
+        else:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if self.player.alive is True:
+                        bullet = Bullet(self.player.rect.x, self.player.rect.y + 30)
+                        self.bullet_group.add(bullet)
+                    if event.key == pygame.K_w:
+                        self.player.jumping = True
+            if event.type == SPAWN_EVENT:
+                for i in range(self.current_enemies):
+                    enemy = Enemy(x=random.randint(680, 750), y=450)
+                    self.enemy_group.add(enemy)
+                self.current_enemies += 3
+            for i in self.bullet_group:
+                i.update()
+                if i.rect.x > 800:
+                    i.kill()
+                for x in self.enemy_group:
+                    if pygame.sprite.collide_rect(x, i):
+                        self.kill_count += 1
+                        x.kill()
+                        i.kill()
+            for i in self.enemy_group:
+                i.update()
+                if pygame.sprite.collide_rect(i, self.player):
+                    self.player.alive = False
+                if i.rect.x < 0:
+                    i.kill()
+            self.player.update()
+            self.player.jump()
+
+    def draw(self, screen):
+        if self.menu.in_menu is True:
+            self.menu.draw(screen)
+        else:
+            text = f'Вы убили {self.kill_count} врагов'
+            rendered = font.render(text, True, (255, 0, 0))
+            screen.blit(bg_image, (0, 0))
+            screen.blit(rendered, (WIDTH - rendered.get_width(), 0))
+            for i in self.bullet_group:
+                i.draw(screen)
+            for i in self.enemy_group:
+                i.draw(screen)
+            self.player.draw(screen)
+
+
+game = Game()
+
+
 while True:
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
             pygame.quit()
-        if in_menu is True and event.type == pygame.MOUSEBUTTONDOWN:
-            in_menu = False
-        if in_menu is True and event.type == pygame.MOUSEMOTION:
-            pos = pygame.mouse.get_pos()
-            if  300 <= pos[0] <= 530 and 270 <= pos[1] <= 320:
-                pressed = True
-            else:
-                pressed = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if in_menu is False:
-                    if player.alive is True:
-                        bullet = Bullet(player.rect.x, player.rect.y + 30)
-                        bullet_group.add(bullet)
-                    if event.key == pygame.K_w:
-                        player.jumping = True
-        if event.type == SPAWN_EVENT:
-            if in_menu is False:
-                for i in range(enemy_count):
-                    enemy = Enemy(x=random.randint(680, 750), y=450)
-                    enemy_group.add(enemy)
-                enemy_count += 3
-    if in_menu is False:
-        for i in bullet_group:
-            i.update()
-            if i.rect.x > 800:
-                i.kill()
-            for x in enemy_group:
-                if pygame.sprite.collide_rect(x, i):
-                    kill_count += 1
-                    x.kill()
-                    i.kill()
-        for i in enemy_group:
-            i.update()
-            if pygame.sprite.collide_rect(i, player):
-                player.alive = False
-            if i.rect.x < 0:
-                i.kill()
-        player.update()
-        player.jump()
-        text = f'Вы убили {kill_count} врагов'
-        rendered = font.render(text, True, (255, 0, 0))
-        screen.blit(bg_image, (0, 0))
-        screen.blit(rendered, (WIDTH - rendered.get_width(), 0))
-        for i in bullet_group:
-            i.draw(screen)
-        for i in enemy_group:
-            i.draw(screen)
-        player.draw(screen)
-    else:
-        screen.fill((0, 0, 0))
-        screen.blit(btn_menu, (330, 150))
-        if pressed is False:
-            pygame.draw.rect(screen, (255, 0, 0), start_btn)
-            screen.blit(btn_text, (300, 270))
-        else:
-            pygame.draw.rect(screen, (0, 0, 255), start_btn)
-            screen.blit(btn_text, (300, 270))
-
+        game.update(event)
+    game.draw(screen)
     clock.tick(60)
     pygame.display.flip()
